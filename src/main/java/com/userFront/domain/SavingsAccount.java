c73c8d3f1326f2e8f90dfb94bd6ac528bfe5ec14
ryan <ryan@192.168.2.158>
2018-05-26 04:58:39 -0400

    First Commit

---
+++ src/main/java/com/userFront/domain/SavingsAccount.java
@@ -0,0 +1,42 @@
+package com.userFront.domain;
+
+import java.util.List;
+import java.math.BigDecimal;
+
+public class SavingsAccount {
+	private Long id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+    private List<SavingsTransaction> savingsTransactionList;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public BigDecimal getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(BigDecimal accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public List<SavingsTransaction> getSavingsTransactionList() {
+		return savingsTransactionList;
+	}
+	public void setSavingsTransactionList(List<SavingsTransaction> savingsTransactionList) {
+		this.savingsTransactionList = savingsTransactionList;
+	}
+	@Override
+	public String toString() {
+		return "SavingsAccount [id=" + id + ", accountNumber=" + accountNumber + ", accountBalance=" + accountBalance
+				+ "]";
+	}
+
+
+}
